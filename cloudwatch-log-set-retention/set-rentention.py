c3a0b53cf30d69caac3aa65637e28e27109a4346
Deepak Dalavi <deepakdalavi@Deepaks-MacBook-Pro.local>
2019-09-27 11:37:19 +0530

    pagination + iam poilcy

--- cloudwatch-log-set-retention/set-rentention.py
+++ cloudwatch-log-set-retention/set-rentention.py
@@ -4,18 +4,24 @@ import boto3
 import argparse
 
 def get_log_group_names(logGroupNamePrefix):
-	log_groups_names = []
-	response = client.describe_log_groups(logGroupNamePrefix=logGroupNamePrefix, limit=50)
-	for record in response['logGroups']:
-		log_groups_names.append(record['logGroupName'])
-	return log_groups_names
+	logGroupNames = []
+	client = boto3.client('logs')
+	paginator = client.get_paginator('describe_log_groups')
+
+	for response in  paginator.paginate(logGroupNamePrefix=logGroupNamePrefix):
+		for record in response['logGroups']:
+			logGroupNames.append(record['logGroupName'])
+	return logGroupNames
 
 def set_retention(logGroupName, retentionInDays):
+	client = boto3.client('logs')
 	try:
-		client.put_retention_policy(retentionInDays, logGroupName)
+		client.put_retention_policy(logGroupName=logGroupName, retentionInDays=retentionInDays)
+		print("Success")
 		return "Success"
-	except Exception:
-		return Exception
+	except Exception as e:
+		print("Error", e)
+		return e
 
 
 
@@ -27,13 +33,12 @@ if __name__ == '__main__':
 	parser.add_argument("-r", "--retention_days",required=True, help="Retention Days")
 
 	args = parser.parse_args()
-
-	client = boto3.client('logs')
 	log_prefix = args.log_group_prefix
 	retention_days = args.retention_days
 	log_groups = get_log_group_names(log_prefix)
+	print(len(log_groups))
 	for name in log_groups:
-		set_retention(name, retention_days)
+		set_retention(name, int(retention_days))
 		print("Retention set to {} days for log group {}".format(retention_days, name))
 
 
